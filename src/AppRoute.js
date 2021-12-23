d5507f3bd37da2cc1a58ad1e9f0b98ae90f54884
Xu <matt.xu@refinitiv.com>
2021-12-23 11:14:54 +0800

    up

--- src/AppRoute.js
+++ src/AppRoute.js
@@ -1,5 +1,4 @@
 import {
-  Routes,
   Route
 } from "react-router-dom";
 import React from 'react';
@@ -8,10 +7,10 @@ import User from "./pages/User";
 
 function AppRoute() {
 	return (
-		<Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="user" element={<User />} />
-    </Routes>
+		<div>
+      <Route exact path="/" component={Home} />
+      <Route path="/user" component={User} />
+    </div>
 	)
 }
 
--- src/index.server.js
+++ src/index.server.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { renderToString } from 'react-dom/server'
 import { Provider } from 'react-redux'
 import { createStore } from './redux/store'
-import { StaticRouter } from 'react-router-dom/server'
+import { StaticRouter } from 'react-router-dom'
 import App from './App'
 import { getUsers } from './services/userService'
 
